1f07bf38ac1db34e856dd17f8eaa7a604756635a
minwoo <jmw041615@gmail.com>
2025-07-01 18:12:23 +0900

    스페셜오더 swiper중

--- scripts/index.js
+++ scripts/index.js
@@ -1,22 +1,22 @@
-/* const swiper = new Swiper('.banner',{
-    navigation:{
-        prevEl:'.btnWrap .swiperButton_prev',
-        nextEl:'.btnWrap .swiperButton_next',
-    },
-},); */
-
-
 const bnr1 = new Swiper('.banner', {
     slidesPerView:2.5,
     loop:true,
-    centeredSlides:true,
-    spaceBetween:30,
     navigation: {
-        prevEl: '.banner_wrap .swiper-button-prev',
-        nextEl: '.banner_wrap .swiper-button-next',
+        prevEl: ".banner_btn_wrap .pagination_arrow_group .swiper-arrow_wrap .swiper-button-prev",
+        nextEl: ".banner_btn_wrap .pagination_arrow_group .swiper-arrow_wrap .swiper-button-next",
+    },
+    pagination: {
+        el: ".banner_btn_wrap .pagination_arrow_group .swiper-pagination",
+        type: "fraction",
     },
     scrollbar:{
-        el:'.banner_wrap .swiper-scrollbar',
-    }
+        el:'.banner_btn_wrap .swiper-scrollbar',
+    },
 });
 
+const specialBnr = new Swiper('.special_order .swiper',{
+    slidesPerView:3,
+    navigation:{
+        prevEl:''.
+    },
+})
