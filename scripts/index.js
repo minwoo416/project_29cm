646d56a9acbfc0e3c2c76f46f40e9248eaf9e0a4
minwoo <jmw041615@gmail.com>
2025-07-03 18:11:36 +0900

    hover

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,25 @@
+const wishlistBtns = document.querySelectorAll('.wishlist_plus');
+
+wishlistBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img'); // 버튼 안의 이미지 태그
+
+        if (img.src.includes('favorite_off_icon.png')) {
+            img.src = './images/favorite_on_icon.png';
+            img.alt = '위시리스트 제거';
+        } else {
+            img.src = './images/favorite_off_icon.png';
+            img.alt = '위시리스트 추가';
+        }
+    });
+});
+
+
+
+
+
+
+//-------------스와이퍼 모음-----------
 //배너 스와이퍼
 const bnr1 = new Swiper('.banner', {
     slidesPerView:2.5,
@@ -27,12 +49,74 @@ const specialBtn = new Swiper('.special_order .swiper',{
 //베스트 스와이퍼
 const bestBtn = new Swiper('.best .swiper',{
     navigation:{
-        prevEl:".best .swiper .best_btn .swiper-button-prev",
-        nextEl:".best .swiper .best_btn .swiper-button-next",
+        prevEl:".best .best_btn .swiper-button-prev",
+        nextEl:".best .best_btn .swiper-button-next",
     },
     pagination: {
-        el: ".best .swiper .best_btn .swiper-pagination",
+        el: ".best .best_btn .swiper-pagination",
         type: "fraction",
     },
+    slidesPerView:1,
 })
-
+/* 베스트 버튼클릭스 카테고리 이동 */
+/* li잡기 */
+const bestCategory = document.querySelectorAll('.best .best_category ul li');
+console.log(bestCategory);
+/* 현재 인덱스에 클래스 추가하기 */
+function updateBestCategory(index) {
+    bestCategory.forEach((li, i) => {
+        li.classList.toggle('active1', i === index);
+    });
+}
+// 슬라이드 변경될 때 카테고리도 바뀌게
+bestBtn.on('slideChange', () => {
+    updateBestCategory(bestBtn.activeIndex);
+});
+// 카테고리 누르면 해당 슬라이드로 이동
+bestCategory.forEach((li, i) => {
+    li.addEventListener('click', () => {
+        bestBtn.slideTo(i);
+        updateBestCategory(i);
+    });
+});
+updateBestCategory(bestBtn.activeIndex);
+//브랜드
+const brandAuto = new Swiper('.brand .swiper',{
+    autoplay:{delay:2000},
+    slidesPerView:3.5,
+    loop:true,
+})
+//뉴 스와이퍼
+const newBtn = new Swiper('.new .swiper',{
+    navigation:{
+        prevEl:".new .new_product_container .new_btn .swiper-button-prev",
+        nextEl:".new .new_product_container .new_btn .swiper-button-next",
+    },
+    pagination: {
+        el: ".new .new_product_container .new_btn .swiper-pagination",
+        type: "fraction",
+    },
+    slidesPerView:1,
+})
+/* 뉴 버튼클릭스 카테고리 이동 */
+/* li잡기 */
+const newCategory = document.querySelectorAll('.new .new_category ul li');
+console.log(newCategory);
+/* 현재 인덱스에 클래스 추가하기 */
+function updateNewCategory(index) {
+    newCategory.forEach((li, i) => {
+        li.classList.toggle('active2', i === index);
+    });
+}
+// 슬라이드 변경될 때 카테고리도 바뀌게
+newBtn.on('slideChange', () => {
+    updateNewCategory(newBtn.activeIndex);
+});
+// 카테고리 누르면 해당 슬라이드로 이동
+newCategory.forEach((li, i) => {
+    li.addEventListener('click', () => {
+        newBtn.slideTo(i);
+        updateNewCategory(i);
+    });
+});
+updateNewCategory(newBtn.activeIndex);
