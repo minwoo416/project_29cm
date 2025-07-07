78bea044e24dfa8e49cad1874d785614096cba4f
minwoo <jmw041615@gmail.com>
2025-07-07 16:44:55 +0900

    제품 더보기 서브페이즈 완료

---
+++ scripts/more.js
@@ -0,0 +1,43 @@
+const wishlistBtns = document.querySelectorAll('.wishlist_plus');
+const longBanner = document.querySelector('header .long_banner');
+const longBannerClose = document.querySelector('header .long_banner .close');
+const category = document.querySelectorAll('.category_wrap .category li');
+
+
+category.forEach((obj, idx)=>{
+    obj.addEventListener('click',()=>{
+        category.forEach((li)=>{li.classList.remove('active')})
+        obj.classList.add('active')
+    })
+})
+console.log(category)
+longBannerClose.addEventListener('click',()=>{
+    longBanner.style.display ='none'
+    lnbWrap.style.marginBottom = '10px'
+})
+
+wishlistBtns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        const img = btn.querySelector('img'); // 버튼 안의 이미지 태그
+
+        if (img.src.includes('favorite_off_icon.png')) {
+            img.src = './images/favorite_on_icon.png';
+            img.alt = '위시리스트 제거';
+        } else {
+            img.src = './images/favorite_off_icon.png';
+            img.alt = '위시리스트 추가';
+        }
+    });
+});
+
+const snb = document.querySelectorAll('.snb > li');
+const snbDepth = document.querySelectorAll('.snb .snb_depth');
+console.log(snb, snbDepth)
+snb.forEach((obj, idx)=>{
+    obj.addEventListener('mouseover',()=>{
+        snbDepth[idx].style.display = 'flex';
+    })
+    obj.addEventListener('mouseout',()=>{
+        snbDepth[idx].style.display = 'none';
+    })
+})
